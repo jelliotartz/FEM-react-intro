869751aa54fe68192cf7cf9678dfdd418e37f564
jelliotartz <jelliotartz@gmail.com>
2017-04-24 18:23:17 -0500

    Convert MyTitle component to JSX.

--- js/MyTitle.js
+++ js/MyTitle.js
@@ -1,18 +1,17 @@
-// ES6 import module syntax
 import React from 'react'
-
-var div = React.DOM.div
-var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
   render: function () {
+    const style = {color: this.props.color}
     return (
-      div(null,
-        h1({ style: { color: this.props.color, fontWeight: 'bold' } }, this.props.title)
-      )
+      <div>
+        <h1 style= { style }>
+          {this.props.title}
+        </h1>
+      </div>
     )
   }
 })
 
-// export ES6 module.
+// export ES6 module
 export default MyTitle
