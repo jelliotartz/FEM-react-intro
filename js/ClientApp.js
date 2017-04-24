c3003c0c1894d1d35a63519ec39227e0f796bad3
jelliotartz <jelliotartz@gmail.com>
2017-04-24 13:31:34 -0500

    Add child component, title and color props, and use createFactory.

---
+++ js/ClientApp.js
@@ -0,0 +1,29 @@
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        h1({style: { color: this.props.color, fontWeight: 'bold' } }, this.props.title)
+      )
+    )
+  }
+})
+
+// use createFactory for cleaner code in MyFirstComponent. Just a convenience method.
+var MyTitleFactory = React.createFactory(MyTitle)
+
+var MyFirstComponent = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        MyTitleFactory({title: 'check out my mad props', color: 'peru'}),
+        MyTitleFactory({title: 'izzy bear', color: 'thistle'}),
+        MyTitleFactory({title: 'prop #3 :)', color: 'rebeccapurple'})
+      )
+    )
+  }
+})
+
+ReactDOM.render(React.createElement(MyFirstComponent), document.getElementById('app'))
