9c113f836e623651e0b2269cec9ff94473fd4373
jelliotartz <jelliotartz@gmail.com>
2017-04-24 15:36:10 -0500

    Add webpack.

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,18 +1,8 @@
-// http://eslint.org/docs/user-guide/configuring#specifying-globals
-/* global React ReactDOM */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MyTitle from './MyTitle'
 
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render: function () {
-    return (
-      div(null,
-        h1({ style: { color: this.props.color, fontWeight: 'bold' } }, this.props.title)
-      )
-    )
-  }
-})
 
 // use createFactory for cleaner code in MyFirstComponent. Just a convenience method.
 var MyTitleFactory = React.createFactory(MyTitle)
---
+++ js/MyTitle.js
@@ -0,0 +1,18 @@
+// ES6 import module syntax
+import React from 'react'
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render: function () {
+    return (
+      div(null,
+        h1({ style: { color: this.props.color, fontWeight: 'bold' } }, this.props.title)
+      )
+    )
+  }
+})
+
+// export ES6 module.
+export default MyTitle
