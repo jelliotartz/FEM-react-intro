e9ea34114e9c72f6e77991684ee0c59cd876a43f
jelliotartz <jelliotartz@gmail.com>
2017-04-25 16:24:03 -0500

    Add ShowCard component.

--- js/ShowCard.js
+++ js/ShowCard.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 const ShowCard = React.createClass({
   render () {
+    // ES6 destructuring
+    const { poster, title, year, description } = this.props.show
     return (
-
+      <div className='show-card'>
+        <img src={`/public/img/posters/${poster}`} />
+        <div>
+          <h3>{title}</h3>
+          <h4>({year})</h4>
+          <p>{description}</p>
+        </div>
+      </div>
     )
   }
 })
+
+export default ShowCard
