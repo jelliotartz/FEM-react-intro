642b56f93b812af5986535a5b899f247b5b084d4
jelliotartz <jelliotartz@gmail.com>
2017-04-26 14:05:11 -0500

    Conditional display logic for search bar.

--- js/Header.js
+++ js/Header.js
@@ -4,6 +4,19 @@ import { Link } from 'react-router'
 // ES6 class syntax
 class Header extends React.Component {
   render () {
+    // utilSpace is either the search input box, or a link to the search page. will be rendered where searchbar goes.
+    let utilSpace
+    if (this.props.showSearch) {
+      utilSpace = <input onChange={this.props.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='search' />
+    } else {
+      utilSpace = (
+        <h2>
+          <Link to='/search'>
+            back
+          </Link>
+        </h2>
+      )
+    }
     return (
       <header>
         <h1>
@@ -11,9 +24,17 @@ class Header extends React.Component {
             Bizzy Bear's Video Emporium
           </Link>
         </h1>
+        {utilSpace}
       </header>
     )
   }
+}
+
+const { func, bool, string } = React.PropTypes
+Header.propTypes = {
+  handleSearchTermChange: func,
+  showSearch: bool,
+  searchTerm: string
 }
 
 export default Header
--- js/Search.js
+++ js/Search.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ShowCard from './ShowCard'
+import Header from './Header'
 const { arrayOf, shape, string } = React.PropTypes
 
 const Search = React.createClass({
@@ -20,10 +21,12 @@ const Search = React.createClass({
   render () {
     return (
       <div className='search'>
-        <header>
-          <h1>bizzy bear's video emporium</h1>
-          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='search' />
-        </header>
+        {/* showSearch={true} is implied by only writing showSearch */}
+        <Header
+          showSearch
+          searchTerm={this.state.searchTerm}
+          handleSearchTermChange={this.handleSearchTermChange}
+        />
         <div>
           {this.props.shows
             .filter((show) => {
