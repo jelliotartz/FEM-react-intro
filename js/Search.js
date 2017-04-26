f62d1996f28cd9700a5720d8111a7f93b4f0eb4c
jelliotartz <jelliotartz@gmail.com>
2017-04-25 19:44:33 -0500

    Handle event state for searchTerm.

--- js/Search.js
+++ js/Search.js
@@ -3,14 +3,28 @@ import preload from '../public/data.json'
 import ShowCard from './ShowCard'
 
 const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: ''
+    }
+  },
+  handleSearchTermChange (event) {
+    this.setState({searchTerm: event.target.value})
+  },
   render () {
     return (
       <div className='search'>
-        {preload.shows.map((show) => {
-          return (
-            <ShowCard key={show.imdbID} {...show} />
-          )
-        })}
+        <header>
+          <h1>bizzy bear's video emporium</h1>
+          <input onChange={this.handleSearchTermChange} value={this.state.searchTerm} type='text' placeholder='search' />
+        </header>
+        <div>
+          {preload.shows.map((show) => {
+            return (
+              <ShowCard key={show.imdbID} {...show} />
+            )
+          })}
+        </div>
       </div>
     )
   }
