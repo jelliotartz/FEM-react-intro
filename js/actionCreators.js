1e34d6e8d17ae5634c76950f99369832e86f307e
jelliotartz <jelliotartz@gmail.com>
2017-04-27 17:26:16 -0500

    Create basic action and actionCreators.

---
+++ js/actionCreators.js
@@ -0,0 +1,8 @@
+// need curly braces here because SET_SEARCH_TERM is an ES6 named export
+import { SET_SEARCH_TERM } from './actions'
+
+// a function that you call, and it gives you back a correctly shaped action
+export function setSearchTerm (searchTerm) {
+  // ES6 shortened syntax return { type: SET_SEARCH_TERM, searchTerm  }
+  return { type: SET_SEARCH_TERM, searchTerm: searchTerm }
+}
---
+++ js/actions.js
@@ -0,0 +1,4 @@
+// ES6 named export
+export const SET_SEARCH_TERM = 'SET_SEARCH_TERM'
+
+
