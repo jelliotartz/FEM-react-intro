14b7dccf046821d042bfa61e1fed4c484285e567
jelliotartz <jelliotartz@gmail.com>
2017-04-28 12:48:59 -0500

    Add redux to search input in Header.

--- js/Header.js
+++ js/Header.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { setSearchTerm } from './actionCreators'
 import { Link } from 'react-router'
 
 // ES6 class syntax
 class Header extends React.Component {
+  constructor (props) {
+    // ensure that this refers to Header when we need it to via constructor - super(props) sets props to inherit from Header
+    super(props)
+    // .bind(this) on handleSearchTermChange
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this)
+  }
+  handleSearchTermChange (event) {
+    this.props.dispatch(setSearchTerm(event.target.value))
+  }
+  // no comma needed after handleSearchTermChange because this is ES6 class syntax
   render () {
     // utilSpace is either the search input box, or a link to the search page. will be rendered where searchbar goes.
     let utilSpace
     if (this.props.showSearch) {
-      utilSpace = <input onChange={this.props.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='search' />
+      utilSpace = <input onChange={this.handleSearchTermChange} value={this.props.searchTerm} type='text' placeholder='search' />
     } else {
       utilSpace = (
         <h2>
@@ -32,9 +44,15 @@ class Header extends React.Component {
 
 const { func, bool, string } = React.PropTypes
 Header.propTypes = {
-  handleSearchTermChange: func,
+  dispatch: func,
   showSearch: bool,
   searchTerm: string
 }
 
-export default Header
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+export default connect(mapStateToProps)(Header)
